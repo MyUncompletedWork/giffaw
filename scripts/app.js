17b0b3079983b4f2c6374395201a6c1bb36187f4
pengfei <pengfeiye1994@gmail.com>
2017-10-03 20:20:18 -0700

    finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
+var endPoint= "http://api.giphy.com/v1/gifs/search" ;
+
 $(document).on("ready", function(){
+
+  $("form").on("submit", function(event){
+    event.preventDefault();
+    $.ajax({
+    method: 'GET',
+    url: endPoint,
+    data:$("form").serialize(),
+    dataType: 'json',
+    success: onSuccess,
+  })}
+
+)
+
+  function onSuccess(responseData){
+    console.log(responseData)
+    responseData.data.forEach(function(link){
+      $(".gif-gallery").append($('<image src='+link.images.fixed_height.url+'>'))
+    })
+}
+
+
+  function onError(x,y,z){
+    $(".gif-gallery").append('<p>'+x+'<p>'+'<p>'+y+'<p>'+'<p>'+z+'<p>')
+  }
 
 });
